faec8c131977de55c83e3f4a29a0e3da732fee6d
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 21:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var obj = Object.assign({}, object);
-  delete obj.key;
-  return obj;
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
