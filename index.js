d652c6cd402907d72f089ab4939f47e00ed60ca8
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 21:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var obj = Object.assign({}, object);
-  delete obj[key];
-  return object;
+  delete obj.key;
+  return obj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
